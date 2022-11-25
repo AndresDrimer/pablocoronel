db435aaca4ba60de678020acdbd75477e1d543e5
AndresDrimer <andresdrimer@hotmail.com>
2022-11-25 16:23:59 -0300

    boton aboutme y awards funcionales

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+
+let btnAboutme = document.getElementById("btn-bio");
+let btnAwards = document.getElementById("btn-awards");
+
+let awardsSection = document.querySelector("#awards");
+let aboutSection = document.querySelector("#aboutme");
+
+btnAwards.addEventListener("click", showAwards);
+btnAboutme.addEventListener("click", showAboutme);
+
+
+function showAwards(){
+   
+    awardsSection.style.display="block";
+    aboutSection.style.display="none"
+   
+}
+function showAboutme(){
+    
+    awardsSection.style.display="none";
+    aboutSection.style.display="block"
+}
